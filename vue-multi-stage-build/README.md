0c641a47ddc72705072de9d507091567ec332c3c
Tomoki Sato <nck.cd.339@gmail.com>
2022-03-13 17:59:57 +0900

    Add the project for vue.js

---
+++ vue-multi-stage-build/README.md
@@ -0,0 +1,3 @@
+# vue-multi-stage-build
+
+TBD
---
+++ vue-multi-stage-build/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <h1>Hello</h1>
+    <div class="greeting">{{ message }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+type DataType = {
+    message: string
+}
+
+const App = defineComponent({
+	data(): DataType {
+		return {
+			message: '',
+		};
+	},
+	created() {
+		this.initMessage();
+	},
+	methods: {
+		initMessage(): void {
+			this.message = 'World !!';
+		}
+	}
+});
+export default App;
+</script>
+
+<style scoped>
+.greeting {
+    color: red;
+    font-weight: bold;
+}
+</style>
